d919509e67ba921a6d944477d75566b18239986a
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-03-06 21:47:32 +0800

    修改小專案

--- src/onlineModel/PlatformVO.java
+++ src/onlineModel/PlatformVO.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class PlatformVO { //接口資料的bean
 	private String platform_name;
-	private String platform_url;
-    private String platform_id;
 
+    private String platform_url;
+
+    private String platform_id;
+    private String merchant_no_tips;
+    private String merchant_pwd_name;
+    private String merchant_pwd_tips;
+    private String platform_no_name;
+    private String platform_no_tips;
+    private String rsa_merchant_private_key_tips;
+    private String rsa_server_public_key_tips;
 
     @Override
     public String toString() {
@@ -16,8 +24,72 @@ public class PlatformVO { //接口資料的bean
                 "platform_name='" + platform_name + '\'' +
                 ", platform_url='" + platform_url + '\'' +
                 ", platform_id='" + platform_id + '\'' +
+                ", merchant_no_tips='" + merchant_no_tips + '\'' +
+                ", merchant_pwd_name='" + merchant_pwd_name + '\'' +
+                ", merchant_pwd_tips='" + merchant_pwd_tips + '\'' +
+                ", platform_no_name='" + platform_no_name + '\'' +
+                ", platform_no_tips='" + platform_no_tips + '\'' +
+                ", rsa_merchant_private_key_tips='" + rsa_merchant_private_key_tips + '\'' +
+                ", rsa_server_public_key_tips='" + rsa_server_public_key_tips + '\'' +
                 '}';
     }
+
+    public String getMerchant_no_tips() {
+        return merchant_no_tips;
+    }
+
+    public void setMerchant_no_tips(String merchant_no_tips) {
+        this.merchant_no_tips = merchant_no_tips;
+    }
+
+    public String getMerchant_pwd_name() {
+        return merchant_pwd_name;
+    }
+
+    public void setMerchant_pwd_name(String merchant_pwd_name) {
+        this.merchant_pwd_name = merchant_pwd_name;
+    }
+
+    public String getMerchant_pwd_tips() {
+        return merchant_pwd_tips;
+    }
+
+    public void setMerchant_pwd_tips(String merchant_pwd_tips) {
+        this.merchant_pwd_tips = merchant_pwd_tips;
+    }
+
+    public String getPlatform_no_name() {
+        return platform_no_name;
+    }
+
+    public void setPlatform_no_name(String platform_no_name) {
+        this.platform_no_name = platform_no_name;
+    }
+
+    public String getPlatform_no_tips() {
+        return platform_no_tips;
+    }
+
+    public void setPlatform_no_tips(String platform_no_tips) {
+        this.platform_no_tips = platform_no_tips;
+    }
+
+    public String getRsa_merchant_private_key_tips() {
+        return rsa_merchant_private_key_tips;
+    }
+
+    public void setRsa_merchant_private_key_tips(String rsa_merchant_private_key_tips) {
+        this.rsa_merchant_private_key_tips = rsa_merchant_private_key_tips;
+    }
+
+    public String getRsa_server_public_key_tips() {
+        return rsa_server_public_key_tips;
+    }
+
+    public void setRsa_server_public_key_tips(String rsa_server_public_key_tips) {
+        this.rsa_server_public_key_tips = rsa_server_public_key_tips;
+    }
+
 
     public String getPlatform_name() {
         return platform_name;
